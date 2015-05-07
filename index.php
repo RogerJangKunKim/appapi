c82407ca2de8bbe6af09b6cad2bb4ed675648dd4
Roger JangKun Kim <rb2stk@gmail.com>
2015-05-07 10:59:54 -0700

    Videos 1 and 2 and 3

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+//Configuration for our PHP server
+set_time_limit(0);
+ini_set('default_socket_timeout', 300);
+session_start();
+
+//Make constants using define.
+define('client_ID', '9ae4c7b4dbaa40a980b5cfa58e733a28');
+define('client_Secret', '42f5be596d8a4151897df655b413a9f0');
+define('redirectURI', 'http://localhost/appapi/index.php');
+define('ImageDirectory', 'pics/');
+?>
+<!-- 
+CLIENT ID	    9ae4c7b4dbaa40a980b5cfa58e733a28
+CLIENT SECRET	42f5be596d8a4151897df655b413a9f0
+WEBSITE URL	    http://localhost/appapi/index.php
+REDIRECT URI	http://localhost/appapi/index.php
+ -->
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title></title>
+ </head>
+ <body>
+ 	<a href="https:api.instagram/oauth.authorize/?client_id=<?php echo client_ID; ?>5redirect_uri=<?php echo redirectURI; ?>5response_type=code/">LOGIN</a>
+ 	<script src="js/main.js"></script>
+ </body>
+ </html>
