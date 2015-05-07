be4289ad29f32998454fab98b3d7836c0179c92c
Roger JangKun Kim <rb2stk@gmail.com>
2015-05-07 11:12:26 -0700

    Video 5.

--- index.php
+++ index.php
@@ -9,6 +9,17 @@ define('clientID', '9ae4c7b4dbaa40a980b5cfa58e733a28');
 define('client_Secret', '42f5be596d8a4151897df655b413a9f0');
 define('redirectURI', 'http://localhost/appapi/index.php');
 define('ImageDirectory', 'pics/');
+
+if isset(($_GET['code'])){
+	$code = ($_GET['code']);
+	$url = 'https://api.instagram.com/oauth/access_token';
+	$access_token_setting = array('client_id' => clientID,
+		'client_secret' =>clientSecret,
+		'grant_type' => 'authorizeation_code',
+		'redirect_uri' => redirectURI,
+		'code' => $code
+		);
+}
 ?>
 <!-- 
 CLIENT ID	    9ae4c7b4dbaa40a980b5cfa58e733a28
