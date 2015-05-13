5f40485818edd10f9e26483abad3e59621dd15b7
Roger JangKun Kim <rb2stk@gmail.com>
2015-05-13 10:51:47 -0700

    Video 7,8,9

--- index.php
+++ index.php
@@ -6,20 +6,48 @@ session_start();
 
 //Make constants using define.
 define('clientID', '9ae4c7b4dbaa40a980b5cfa58e733a28');
-define('client_Secret', '42f5be596d8a4151897df655b413a9f0');
+define('clientSecret', '42f5be596d8a4151897df655b413a9f0');
 define('redirectURI', 'http://localhost/appapi/index.php');
 define('ImageDirectory', 'pics/');
 
+//function that is going to connect to Instagram.
+function connectToInstagram($url){
+	$ch = curl_init();
+
+	curl_setopt($ch, array(
+		CURLOPT_URL => $url,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_SSL_VERIFYPEER => false,
+		CURLOPT_SSL_VERIFYHOST => 2,
+	));
+	$result = curl_exec($ch);
+	curl_close($ch);
+	return $result;
+}
+
 if (isset($_GET['code'])){
 	$code = ($_GET['code']);
-	$url = 'https://api.instagram.com/oauth/access_token';
-	$access_token_setting = array('client_id' => clientID,
+	$url = "https://api.instagram.com/oauth/access_token";
+	$access_token_settings = array('client_id' => clientID,
 		'client_secret' =>clientSecret,
-		'grant_type' => 'authorizeation_code',
+		'grant_type' => 'authorization_code',
 		'redirect_uri' => redirectURI,
 		'code' => $code
 		);
+//library used to make calls to other api's
+$curl = curl_init($url); //getting data from url
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $access_token_settings); //setting to the array that we created
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); //setting to 1 because we are getting string back
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); //in live work-production, we want to set to true
+
+$result = curl_exec($curl);
+curl_close();
+
+$results = json_decode($result, true);
+echo $results['user']['username'];
 }
+else{
 ?>
 <!-- 
 CLIENT ID	    9ae4c7b4dbaa40a980b5cfa58e733a28
@@ -33,7 +61,10 @@ REDIRECT URI	http://localhost/appapi/index.php
  	<title></title>
  </head>
  <body>
- 	<a href="https:api.instagram.com/oauth.authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI; ?>&response_type=code/">LOGIN</a>
+ 	<a href="https:api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI; ?>&response_type=code">LOGIN</a>
  	<script src="js/main.js"></script>
  </body>
  </html>
+ <?php
+}
+?>
